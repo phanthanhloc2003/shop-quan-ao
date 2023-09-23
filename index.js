373f049e8cf01735f1d08e40187a29c441fb8470
phan thanh loc <lopphanskt92@gmail.com>
2023-09-24 03:28:49 +0700

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router('db.json'); // Đảm bảo rằng bạn chuyển tên tệp nếu nó không được đặt tên là db.json
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
-
-
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server đang chạy tại http://localhost:${port}`);
+});
